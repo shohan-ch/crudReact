604800e63dc35210add13564e5d6dd796cd0f3b7
shohan <shohan889@yahoo.com>
2022-06-13 23:56:25 +0600

    Register design initailly done

--- resources/js/pages/Register.jsx
+++ resources/js/pages/Register.jsx
@@ -14,112 +14,145 @@ export default () => {
     };
     return (
         <>
-            <div className="mx-auto max-w-lg shadow-lg py-8 px-8 rounded-md space-y-2 ">
-                <h2 className="text-black text-center font-medium text-2xl">
-                    Registration form
-                </h2>
+            <div className="h-screen bg-sky-200 flex items-center">
+                <div className="mx-auto  py-8 px-8 rounded-lg space-y-2 shadow-xl  bg-white min-w-[20%]">
+                    <h2 className="text-black text-center font-medium text-2xl">
+                        Registration form
+                    </h2>
 
-                <form className="space-y-3">
-                    <div>
-                        <label htmlFor="name" className="block font-medium ">
-                            Name
-                        </label>
-                        <input type="text" className="" placeholder="Name" />
-                    </div>
-                    <div>
-                        <label htmlFor="email" className="block font-medium ">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            className="mt-1"
-                            placeholder="Email"
-                        />
-                    </div>
+                    <form
+                        className="space-y-3"
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <div>
+                            <label
+                                htmlFor="name"
+                                className="block font-medium "
+                            >
+                                Name
+                            </label>
+                            <input
+                                type="text"
+                                className=""
+                                placeholder="Name"
+                            />
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="email"
+                                className="block font-medium "
+                            >
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                className="mt-1"
+                                placeholder="Email"
+                            />
+                        </div>
 
-                    <div>
-                        <label htmlFor="gender" className="block font-medium ">
-                            Gender
-                        </label>
-                        <select
-                            name="gender"
-                            id="gender"
-                            className="w-full rounded-md py-1 focus:border-indigo-500"
+                        <div>
+                            <label
+                                htmlFor="gender"
+                                className="block font-medium "
+                            >
+                                Gender
+                            </label>
+                            <select
+                                name="gender"
+                                id="gender"
+                                className="w-full rounded-md py-1 focus:border-indigo-500"
+                            >
+                                <option value="">Select</option>
+                                <option value="m">Male</option>
+                                <option value="f">Female</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <input
+                                type="checkbox"
+                                name="checkBox"
+                                className=" focus:ring-indigo-700 rounded focus:ring-1 "
+                            />
+                            <span
+                                htmlFor="checkbox"
+                                className="font-medium mt-1 ml-2"
+                            >
+                                Accept terms and conditions.
+                            </span>
+                        </div>
+                        <button
+                            className="my-btn py-1 bg-red-700"
+                            type="submit"
                         >
-                            <option value="">Select</option>
-                            <option value="m">Male</option>
-                            <option value="f">Female</option>
-                        </select>
-                    </div>
-                </form>
-            </div>
+                            Register
+                        </button>
+                    </form>
+                </div>
 
-            <div>
-                <p className="text-indigo-800 mt-9">Name</p>
-                <div className="card">
-                    <div className="card-header">
-                        <h2 className="text-indigo-200 bg-stone-700">
-                            Please Register here
-                        </h2>
-                    </div>
-                    <div className="card-body">
-                        <div style={style.formDiv}>
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                                <div className="form-group">
-                                    <label
-                                        htmlFor="name"
-                                        className="text-slate-500"
+                {/* <div>
+                    <div className="card">
+                        <div className="card-header"></div>
+                        <div className="card-body">
+                            <div style={style.formDiv}>
+                                <form>
+                                    <div className="form-group">
+                                        <label
+                                            htmlFor="name"
+                                            className="text-slate-500"
+                                        >
+                                            Name
+                                        </label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            {...register("name", {
+                                                required: true,
+                                            })}
+                                            id="name"
+                                            placeholder="Name"
+                                        />
+
+                                        {errors.name && (
+                                            <span className="text-danger">
+                                                Name is required
+                                            </span>
+                                        )}
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            {...register("email")}
+                                            id="name"
+                                            placeholder="Email"
+                                        />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="phone">Phone</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            {...register("phone")}
+                                            id="name"
+                                            placeholder="Phone"
+                                        />
+                                    </div>
+                                    <Button
+                                        type="primary"
+                                        htmlType="submit"
+                                        className="btn btn-danger"
                                     >
-                                        Name
-                                    </label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        {...register("name", {
-                                            required: true,
-                                        })}
-                                        id="name"
-                                        placeholder="Name"
-                                    />
-
-                                    {errors.name && (
-                                        <span className="text-danger">
-                                            Name is required
-                                        </span>
-                                    )}
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        {...register("email")}
-                                        id="name"
-                                        placeholder="Email"
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="phone">Phone</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        {...register("phone")}
-                                        id="name"
-                                        placeholder="Phone"
-                                    />
-                                </div>
-                                <Button
-                                    type="primary"
-                                    htmlType="submit"
-                                    className="btn btn-danger"
-                                >
-                                    Submit
-                                </Button>
-                            </form>
+                                        Submit
+                                    </Button>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </>
     );
